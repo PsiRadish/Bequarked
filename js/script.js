9f75f036dd509f5cb4536b7a2768a354c2999384
Kyle Fiegener <psiradish@comcast.net>
2015-07-12 15:55:57 -0700

    Assignment requirements met (I think).
    Gonna make some better animations for matching quarks, though.

--- js/script.js
+++ js/script.js
@@ -14,15 +14,12 @@ function main()
     //var gameView = $(STYLE.sel.gameView);
     var gameView = $('#game');
     
-    Game.init(gameView[0]);
-    
     //var board = $(STYLE.sel.board);
     var board = $('#board');
     
     var playerPanes = $('.player');
     var player1Pane = $('.player-1');
     var player2Pane = $('.player-2');
-    
     
     // size the board appropriately
     function sizeGame(e)
@@ -66,56 +63,329 @@ function main()
     // FitText.js calls for fully responsive text
     $('.player h2').fitText(0.38333333333333333333333333333333); // 30px font size at max game size
     $('.player h3').fitText(0.47916666666666666666666666666667); // 24px font size at max game size
-    //$('#title-container h1').fitText(0.68125);                   // 160px font size at max game size
-    $('#title-container h1').fitText(0.77857142857142857142857142857143);                   // 160px font size at max game size
+    $('#title-container h1').fitText(0.77857142857142857142857142857143);  // 140px font size at max game size
+    $('#winner-display').fitText(0.90825688073394495412844036697248); // 99px font size at max game size
     
-    function initSwap()
+    
+    // callback of doom
+    function removeElement(e)
     {
-        
+        $(this).remove();
     }
     
-    function turnChanged(newTurn, oldTurn)
+    var xCoordRegex = /[^\w-]x([0-7])[^\w-]/;
+    var yCoordRegex = /[^\w-]y([0-7])[^\w-]/;
+    // extract x y grid coordinates as integers from a class attribute value string
+    function extractCoords(classString)
     {
-        playerPanes.removeClass("my-turn");
-        board.removeClass("player-" + oldTurn + "-turn");
+        var coords = {};
         
-        $('#player-' + newTurn).addClass("my-turn");
-        board.addClass("player-" + newTurn + "-turn");
+        coords.x = parseInt(xCoordRegex.exec(classString)[1]);
+        coords.y = parseInt(yCoordRegex.exec(classString)[1]);
+        
+        return coords;
+    }
+    
+    function quarkElementFromGameSquare(gameSquare)
+    {
+        var quarkElement = $('<div class="grid-element quark-container"> <div class="arrow-container"></div></div>');
+        quarkElement.addClass("x" + gameSquare.x + " y" + gameSquare.y);
+        quarkElement.addClass(gameSquare.quark.css);
+        
+        return quarkElement;
+    }
+    
+    function pauseInput()
+    {
+        board.removeClass('taking-input');
+    }
+    function unpauseInput()
+    {
+        board.addClass('taking-input');
+    }
+    
+    var quarkClassRegex = /[^\w-](ring-\w+ +arrow-\w+)/;
+    function doAnimateSwapSuccess($quarkA, $quarkB)
+    {
+        pauseInput();
+        Game.animating = true;
+        
+        var swapDirectionA;
+        var swapDirectionB;
+                
+        if ($quarkA.css('top') == $quarkB.css('top'))
+        {   // horizontal swap
+            if (parseFloat($quarkA.css('left')) > parseFloat($quarkB.css('left')))
+            {
+                swapDirectionA = 'left';
+                swapDirectionB = 'right';
+            }
+            else
+            {
+                swapDirectionA = 'right';
+                swapDirectionB = 'left';
+            }
+        }
+        else
+        {   // vertical swap
+            if (parseFloat($quarkA.css('top')) > parseFloat($quarkB.css('top')))
+            {
+                swapDirectionA = 'up';
+                swapDirectionB = 'down';
+            }
+            else
+            {
+                swapDirectionA = 'down';
+                swapDirectionB = 'up';
+            }
+        }
+        
+        // .swapping.left-swap
+        var animClassesA = 'distorting swapping ' + swapDirectionA + '-swap';
+        var animClassesB = 'distorting swapping ' + swapDirectionB + '-swap';
+        // adding these classes will initiate CSS animations
+        $quarkA.addClass(animClassesA);
+        $quarkB.addClass(animClassesB);
+        
+        // wait for CSS animations to finish
+        window.setTimeout(function()
+        {
+            // clear animation classes
+            $quarkA.removeClass(animClassesA);
+            $quarkB.removeClass(animClassesB);
+            
+            aQuarkClass = quarkClassRegex.exec($quarkA.attr('class'))[1];
+            bQuarkClass = quarkClassRegex.exec($quarkB.attr('class'))[1];
+            
+            // swap the CSS classes that determine which ring and arrow to show
+            $quarkA.removeClass(aQuarkClass);
+            $quarkA.addClass(bQuarkClass);
+            $quarkB.removeClass(bQuarkClass);
+            $quarkB.addClass(aQuarkClass);
+            
+            unpauseInput();
+            Game.animating = false;
+        }, 300);
+    }
+    
+    function animateSwapFail($quarkA, $quarkB)
+    {
+        pauseInput();
+        Game.animating = true;
+        
+        var swapDirectionA;
+        var swapDirectionB;
+                
+        if ($quarkA.css('top') == $quarkB.css('top'))
+        {   // horizontal swap
+            if (parseFloat($quarkA.css('left')) > parseFloat($quarkB.css('left')))
+            {
+                swapDirectionA = 'left';
+                swapDirectionB = 'right';
+            }
+            else
+            {
+                swapDirectionA = 'right';
+                swapDirectionB = 'left';
+            }
+        }
+        else
+        {   // vertical swap
+            if (parseFloat($quarkA.css('top')) > parseFloat($quarkB.css('top')))
+            {
+                swapDirectionA = 'up';
+                swapDirectionB = 'down';
+            }
+            else
+            {
+                swapDirectionA = 'down';
+                swapDirectionB = 'up';
+            }
+        }
+        
+        var animClassesA = 'distorting swap-fail ' + swapDirectionA + '-swap-fail';
+        var animClassesB = 'distorting swap-fail ' + swapDirectionB + '-swap-fail';
+        // adding these classes will initiate CSS animations
+        $quarkA.addClass(animClassesA);
+        $quarkB.addClass(animClassesB);
+        
+        // wait for CSS animations to finish
+        window.setTimeout(function()
+        {
+            // clear animation classes
+            $quarkA.removeClass(animClassesA);
+            $quarkB.removeClass(animClassesB);
+            
+            unpauseInput();
+            Game.animating = false;
+        }, 210);
     }
     
     board.on('click', function(e)
     {
-        if (!Game.animating)
+        if (board.is('.taking-input') && !Game.animating)
         {
-            var clickedQuark = $(e.target).closest(".quark-container");
-                                    
+            var clickedQuark = $(e.target).closest('.quark-container');
+            
             if (clickedQuark.hasClass('selected')) // this quark is already selected
             {
                 // deselect it
                 clickedQuark.removeClass('selected');
                 
-                return;
+                return; // and done
             }
             
-            var selectedQuark = $(".selected");
+            var selectedQuark = $('.selected');
             
             if (!selectedQuark.length) // no quark already selected
             {
                 // select the clicked quark
-                clickedQuark.addClass('selected');
+                clickedQuark.addClass("selected");
+                
+                return; // and done
             }
             else
             {
-                //initSwap();
+                var selectedCoords = extractCoords(selectedQuark.attr('class'));
+                var clickedCoords = extractCoords(clickedQuark.attr('class'));
                 
+                // check for adjacency
+                if ((selectedCoords.x == clickedCoords.x && Math.abs(selectedCoords.y - clickedCoords.y) == 1) ||
+                    (selectedCoords.y == clickedCoords.y && Math.abs(selectedCoords.x - clickedCoords.x) == 1))
+                {
+                    // deselect quarks
+                    selectedQuark.removeClass("selected");
+                    
+                    var selectedSquare = Game.Grid.getSquare(selectedCoords.x, selectedCoords.y);
+                    var clickedSquare = Game.Grid.getSquare(clickedCoords.x, clickedCoords.y);
+                    
+                    // try the swap
+                    Game.Grid.swapQuarks(selectedSquare, clickedSquare);
+                    
+                    return;
+                    // we'll have to wait for an event to learn the results
+                }
+                else // too far away for swap, so select the new quark instead
+                {
+                   selectedQuark.removeClass("selected");
+                   clickedQuark.addClass("selected");
+                }
             }
-            
-            // !!!!! TEMPORARY: TURN CHANGE TEST !!!!!
-            var oldTurn = Game.turn;
-            Game.turn = (oldTurn == 1) ? 2 : 1;
-            
-            turnChanged(Game.turn, oldTurn);
-            // !!!!! TEMPORARY: TURN CHANGE TEST !!!!!
         }
     });
+    
+    // CUSTOM EVENT LISTENERS
+    gameView.on('board:start', function(e, gameSquares)
+    {
+        board.html(''); // clear the board
+        
+        for (var gridKey in gameSquares)
+        {   var currSquare = gameSquares[gridKey];
+            
+            //var quarkElement = quarkElementFromGameSquare(currSquare);
+            
+            quarkElementFromGameSquare(currSquare).appendTo(board);
+        }
+    });
+    
+    gameView.on('swap:success', function(e, squareA, squareB)
+    {
+        var queryA = '.quark-container.x'+squareA.x+'.y'+squareA.y;
+        var queryB = '.quark-container.x'+squareB.x+'.y'+squareB.y;
+        
+        doAnimateSwapSuccess($(queryA), $(queryB));
+    });
+    
+    gameView.on('swap:fail', function(e, squareA, squareB)
+    {
+        var queryA = '.quark-container.x'+squareA.x+'.y'+squareA.y;
+        var queryB = '.quark-container.x'+squareB.x+'.y'+squareB.y;
+        
+        animateSwapFail($(queryA), $(queryB));
+    });
+    
+    gameView.on('matches:remove', function(e, matchData, scoreChain, scoreMult)
+    {
+        // display the score earned for the last match
+        var chainString = scoreChain.toString();
+        if (scoreMult > 1) // only show multiplier if > 1
+            chainString += ' ×' + scoreMult;
+        
+        $('.my-turn .score-chain').html(chainString);
+        
+        pauseInput();
+        Game.animating = true;
+        
+        var animDur = 300;
+        
+        for (var gridKey in matchData)
+        {   var currDatum = matchData[gridKey];
+            var currSquare = currDatum.square;
+            
+            var doomedQuark = $('.quark-container.x' + currSquare.x + '.y' + currSquare.y);
+            
+            // !!!!! Temporary fade out animation
+            doomedQuark.fadeOut(animDur, removeElement);
+        }
+        
+        // wait for animations to finish
+        window.setTimeout(function()
+        {
+            unpauseInput();
+            Game.animating = false;
+        }, animDur);
+    });
+    
+    gameView.on('board:newQuarks', function(e, newSquares)
+    {
+        pauseInput();
+        Game.animating = true;
+        
+        var animDur = 500;
+        
+        newSquares.forEach(function(newSquare)
+        {
+            var newQuark = quarkElementFromGameSquare(newSquare);
+            
+            newQuark.appendTo(board);
+            
+            // !!!!!!!! TEMPORARY FADE ANIM
+            newQuark.fadeIn(animDur);
+        });
+        
+        // wait for animations to finish
+        window.setTimeout(function()
+        {
+            unpauseInput();
+            Game.animating = false;
+        }, animDur);
+    });
+    
+    gameView.on('board:stable', function(e, oldTurn, newTurn, newScore)
+    {
+        $('.my-turn .score').html(newScore);
+        $('.my-turn .score-chain').html('');
+        
+        playerPanes.removeClass("my-turn");
+        board.removeClass("player-" + oldTurn + "-turn");
+        
+        $('#player-' + newTurn).addClass("my-turn");
+        board.addClass("player-" + newTurn + "-turn");
+    });
+    
+    gameView.on('gameOver', function(e, winner)
+    {
+        pauseInput();
+        
+        if (!winner) // tie
+        {
+            gameView.addClass("tie");
+            return;
+        }
+        
+        gameView.addClass("player-" + winner + "-win");
+        $('#player-' + winner + ' .score-chain').html("WINNER");
+    });
+    
+    Game.init(gameView); // let's get this STARTED
 }
